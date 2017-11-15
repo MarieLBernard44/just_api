27158df73aad010930fafe51768ffad0f8d2efaa
Clementine <clementine-tournery@hotmail.fr>
2017-11-15 10:33:08 +0100

    populate database

--- BDD/install.sh
+++ BDD/install.sh
@@ -3,4 +3,5 @@
 /etc/init.d/mysql start
 # create database
 mysql -u root -e "create database just_api;"
-#mysql -u imie --password=passe just_api < dump.sql; 
+sleep 2;
+mysql -u root --password=root just_api < dump.sql; 
