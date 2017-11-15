5163c15f2a0216daf1de2ba308ed578285a15c36
nicoh <nicolas.hersant@gmail.com>
2017-11-15 15:17:28 +0100

    ajout affichage tous

--- view/public/ajax.js
+++ view/public/ajax.js
@@ -1,12 +1,35 @@
 var urlban = 'http://localhost:3000/api/v1/banque'
 var urlass = 'http://localhost:3000/api/v1/assurance'
+var url = 'http://localhost:3000/api/v1/'
+
+getAll()
+
+$( document ).ready(function() {
+$( "#allbut" ).click( function() {
+  $("#tabody").empty()
+  getAll()
+});
+});
+function getAll(){
+$.get(url, function (data) {
+  for (var key in data){
+    var item = data[key];
+    var tr = document.createElement('tr')
+    for (var key in item){
+      var td = document.createElement('td')
+      td.append(item[key])
+      tr.append(td)
+    }
+    $("#tabody").append(tr);
+  }
+})
+}
 
 $( document ).ready(function() {
 
   $( "#banbut" ).click( function() {
     $("#tabody").empty()
     $.get(urlban, function (data) {
-      //console.log(data)
       for (var key in data){
         var item = data[key];
         var tr = document.createElement('tr')
