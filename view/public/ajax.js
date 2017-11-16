1dfdbf89aee8fea3616f57ecafc3a2a02d302540
nicoh <nicolas.hersant@gmail.com>
2017-11-16 12:31:02 +0100

    clear index.pug & ajax.js

--- view/public/ajax.js
+++ view/public/ajax.js
@@ -1,24 +1,6 @@
-var urlban = 'http://localhost:3000/api/v1/banque'
-var urlass = 'http://localhost:3000/api/v1/assurance'
-var urlbat = 'http://localhost:3000/api/v1/batiment'
-var urldev = 'http://localhost:3000/api/v1/developpement'
-var urlsan = 'http://localhost:3000/api/v1/sante'
 var url = 'http://localhost:3000/api/v1/'
 
 getProfil(url)
-
-var activities =
-  $.get(url, function (data) {
-    var activities = []
-      for (var key in data){
-        var item = data[key];
-        activities.push(item.sector)
-      }
-    var tab = Array.from(new Set(activities))
-    console.log(tab)
-    return tab
-  })
-console.log(activities)
 
 function getProfil(url){
   $("#tabody").empty()
@@ -35,42 +17,42 @@ function getProfil(url){
     }
   })
 }
+
 function getLink(act){
-  $("#ddl").empty()
+  $("#dllbut").empty()
   var a = document.createElement('a')
   a.setAttribute("class", "btn btn-info glyphicon glyphicon-save-file")
   a.setAttribute("href", "http://localhost:3000/download/"+act)
   a.innerHTML = 'Téléchargement'
   var p = document.createElement('p')
   p.innerHTML = 'Veuillez cliquer sur le bouton afin de télécharger la liste des profils du secteur : '+act
-  $("#ddl").append(p)
-  $("#ddl").append(a)
+  $("#dllbut").append(p)
+  $("#dllbut").append(a)
 }
 
 $( document ).ready(function() {
   $( "#allbut" ).click( function() {
     getProfil(url)
-    $("#ddl").empty()
-  });
-  $( "#banbut" ).click( function() {
-    getProfil(urlban)
-    getLink('banque')
+    $("#dllbut").empty()
   });
   $( "#assbut" ).click( function() {
-    getProfil(urlass)
+    getProfil(url+'assurance')
     getLink('assurance')
   });
+  $( "#banbut" ).click( function() {
+    getProfil(url+'banque')
+    getLink('banque')
+  });
   $( "#batbut" ).click( function() {
-    getProfil(urlbat)
+    getProfil(url+'batiment')
     getLink('batiment')
   });
   $( "#devbut" ).click( function() {
-    getProfil(urldev)
+    getProfil(url+'developpement')
     getLink('developpement')
   });
   $( "#sanbut" ).click( function() {
-    getProfil(urlsan)
+    getProfil(url+'sante')
     getLink('sante')
   });
-
 });
